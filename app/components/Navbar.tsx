5caeb98f3382bcfe9cd5f970ce4ea52d3fe0d0ab
Sundharabalaji-K-L <narasimhanbalaji13@gmail.com>
2025-09-22 00:33:17 +0530

    added logout button to home

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,24 @@
 import {Link} from "react-router";
+import {usePuterStore} from "~/lib/puter";
 
 const Navbar = () => {
+    const {auth} = usePuterStore();
+
     return (
-        <nav className="navbar">
+        <nav className="navbar flex items-center justify-space-around">
             <Link to="/">
                 <p className="text-2xl font-bold text-gradient">RESUMIND</p>
             </Link>
-            <Link to="/upload" className="primary-button w-fit">
-                Upload Resume
-            </Link>
+           <div className="flex items-center justify-space-between gap-6">
+               <Link to="/upload" className="primary-button w-fit">
+                   Upload Resume
+               </Link>
+
+               <button className="primary-button w-fit" onClick={auth.signOut}>
+                   <p>Log Out</p>
+               </button>
+           </div>
+
         </nav>
     )
 }
